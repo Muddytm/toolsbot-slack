2cbbe11c2f16c25a4b94332ddc0273212c5bd08d
calebh93 <hawkins.caleb93@gmail.com>
2018-12-14 14:48:28 -0800

    Setting this up temporarily for auto-post

--- commands.py
+++ commands.py
@@ -4,8 +4,10 @@ import json
 import os
 from slackbot.bot import respond_to
 import re
+import time
 import urllib.request as urllib2
 
+tls_check = False
 
 @respond_to("test", re.IGNORECASE)
 def test(message):
@@ -74,6 +76,88 @@ def tls(message):
     date = "{}/{}/{}".format(latest_tokens[2], latest_tokens[3], latest_tokens[1])
 
     message.reply("TLS 1.0/1.1 summary for {}:\n```{}```".format(date, topten))
+
+
+@listen_to("starttls", re.IGNORECASE)
+def starttls(message):
+    """Start TLS loop."""
+    if tls_check:
+        return
+    else:
+        while True:
+            if os.path.exists("data/jobs.json"):
+                with open("data/jobs.json") as f:
+                    jobs = json.load(f)
+
+                if "TLS" in jobs:
+                    jobs.remove("TLS")
+                    with open("data/jobs.json", "w") as f:
+                        json.dump(jobs, f)
+                else:
+                    time.sleep(60)
+                    continue
+            else:
+                time.sleep(60)
+                continue
+
+            with open("data/tls.json") as f:
+                data = json.load(f)
+
+            if os.path.exists("data/tls_cache.json"):
+                with open("data/tls_cache.json") as f:
+                    cache = json.load(f)
+            else:
+                #message.reply("No cache to read from. Contact Caleb Hawkins to get this fixed.")
+                return
+
+            total = 0
+            topten = ""
+            results = []
+
+            for ip in data:
+                total += data[ip]
+
+            for i in range(10):
+                name = ""
+                count = 0
+
+                for ip in data:
+                    if data[ip] > count:
+                        name = ip
+                        count = data[ip]
+
+                rename = False
+                org = "Unknown"
+                for set in cache:
+                    if name in cache[set]:
+                        org = set
+                        for other_ip in cache[set]:
+                            if other_ip != name:
+                                rename = True
+                                count += data[other_ip]
+                                del data[other_ip]
+
+                        del cache[set]
+                        break
+
+                del data[name]
+
+                if rename:
+                    name = "Multiple IPs"
+                else:
+                    name = "<https://api.ipdata.co/{}?api-key={}|{}>".format(name,
+                                                                             config.ip_api_key,
+                                                                             name)
+
+                percentage = "%.2f" % float((count/total)*100.)
+                topten += "\n{} ({}): {}%".format(org, name, str(percentage))
+
+            file_list = glob.glob("/mnt/TLS/*.txt")
+            latest = max(file_list, key=os.path.getctime)
+            latest_tokens = latest.replace(".txt", "").split("-")
+            date = "{}/{}/{}".format(latest_tokens[2], latest_tokens[3], latest_tokens[1])
+
+            message.reply("TLS 1.0/1.1 summary for {}:\n```{}```".format(date, topten))
 
 
 @respond_to("start", re.IGNORECASE)
