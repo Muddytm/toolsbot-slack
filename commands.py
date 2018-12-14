df444e457ac5ca938804cc380c2d2ad86978fbb4
calebh93 <hawkins.caleb93@gmail.com>
2018-12-14 11:26:51 -0800

    Rewriting how the results are tallied up...YOLO

--- commands.py
+++ commands.py
@@ -22,7 +22,8 @@ def tls(message):
         with open("data/tls_cache.json") as f:
             cache = json.load(f)
     else:
-        cache = {}
+        message.reply("No cache to read from. Contact Caleb Hawkins to get this fixed.")
+        return
 
     total = 0
     topten = ""
@@ -34,52 +35,72 @@ def tls(message):
     for i in range(10):
         name = ""
         count = 0
-        for ip in data:
-            total += data[ip]
 
+        for ip in data:
             if data[ip] > count:
                 name = ip
                 count = data[ip]
 
-        # Stuff for determining name of IP address owner
-        org = None
-        for orgname in cache:
-            if name in cache[orgname]:
-                org = orgname
-
-        if not org:
-            url = "https://api.ipdata.co/{}?api-key={}".format(name,
-                                                               config.ip_api_key)
-            response = urllib2.urlopen(url)
-            ip_data = response.read()
-            info = json.loads(ip_data)
-
-            org = "Unknown"
-            if "organisation" in info:
-                org = info["organisation"]
-
-            if org not in cache:
-                cache[org] = []
-
-            if name not in cache[org]:
-                cache[org].append(name)
-
-        changed = False
-        for result in results:
-            if result[0] == org:
-                result[1] = "Multiple IPs"
-                result[2] += count
-                changed = True
+        org = "Unknown"
+        for set in cache:
+            if name in cache[set]:
+                org = set
+                for other_ip in cache[set]:
+                    if other_ip != name:
+                        count += data[other_ip]
+                        del data[other_ip]
                 break
 
-        if not changed:
-            results.append([org, name, count])
+        percentage = "%.2f" % float((count/total)*100.)
+        topten += "\n{} ({}): {}%".format(org, name, str(percentage))
 
-        del data[name]
-
-    for result in results:
-        percentage = "%.2f" % float((result[2]/total)*100.)
-        topten += "\n{} ({}): {}%".format(result[0], result[1], str(percentage))
+    # for i in range(10):
+    #     name = ""
+    #     count = 0
+    #     for ip in data:
+    #         if data[ip] > count:
+    #             name = ip
+    #             count = data[ip]
+    #
+    #     # Stuff for determining name of IP address owner
+    #     org = None
+    #     for orgname in cache:
+    #         if name in cache[orgname]:
+    #             org = orgname
+    #
+    #     if not org:
+    #         url = "https://api.ipdata.co/{}?api-key={}".format(name,
+    #                                                            config.ip_api_key)
+    #         response = urllib2.urlopen(url)
+    #         ip_data = response.read()
+    #         info = json.loads(ip_data)
+    #
+    #         org = "Unknown"
+    #         if "organisation" in info:
+    #             org = info["organisation"]
+    #
+    #         if org not in cache:
+    #             cache[org] = []
+    #
+    #         if name not in cache[org]:
+    #             cache[org].append(name)
+    #
+    #     changed = False
+    #     for result in results:
+    #         if result[0] == org:
+    #             result[1] = "Multiple IPs"
+    #             result[2] += count
+    #             changed = True
+    #             break
+    #
+    #     if not changed:
+    #         results.append([org, name, count])
+    #
+    #     del data[name]
+    #
+    # for result in results:
+    #     percentage = "%.2f" % float((result[2]/total)*100.)
+    #     topten += "\n{} ({}): {}%".format(result[0], result[1], str(percentage))
 
 
     message.reply("TLS 1.0/1.1 summary:\n```{}```".format(topten))
--- utilities/get_top_ips.py
+++ utilities/get_top_ips.py
@@ -1,6 +1,7 @@
 import glob
 import json
 import os
+import urllib.request as urllib2
 
 file_list = glob.glob("/mnt/TLS/*.txt")
 latest = max(file_list, key=os.path.getctime)
@@ -19,3 +20,30 @@ for line in lines:
 
 with open("data/tls.json", "w") as f:
     json.dump(data, f)
+
+# Assigning orgs and making cache
+if os.path.exists("data/tls_cache.json"):
+    with open("data/tls_cache.json") as f:
+        cache = json.load(f)
+else:
+    cache = {}
+
+for ip in data:
+    url = "https://api.ipdata.co/{}?api-key={}".format(name,
+                                                       config.ip_api_key)
+    response = urllib2.urlopen(url)
+    ip_data = response.read()
+    info = json.loads(ip_data)
+
+    org = "Unknown"
+    if "organisation" in info:
+        org = info["organisation"]
+
+    if org not in cache:
+        cache[org] = []
+
+    if ip not in cache[org]:
+        cache[org].append(ip)
+
+with open("data/tls_cache.json", "w") as f:
+    json.dump(cache, f)
